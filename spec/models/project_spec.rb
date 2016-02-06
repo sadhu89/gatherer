1d890d085b56af28bf04458af352d947446a932a
Carlos Rojas <carlos.sadhu@gmail.com>
2016-02-06 09:41:24 -0500

    Initial version

---
+++ spec/models/project_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe Project do
+  describe "initializes" do
+    let(:project){Project.new}
+    let(:task){Task.new}
+    it "considers a project with no tasks to be done" do
+      expect(project).to be_done    
+    end
+
+    it "knows that a project with an incomplete task is not be done" do  
+      project.tasks << task
+      expect(project).not_to be_done
+    end
+  end
+end
