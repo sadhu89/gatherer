06b5c76a8b338b46126562484821bff787d72545
Carlos Rojas <carlos.sadhu@gmail.com>
2016-02-13 09:56:35 -0500

    Add task rate completition

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,9 +1,40 @@
 class Project
-  attr_accessor :tasks
+  attr_accessor :tasks, :due_date
   def initialize
     @tasks=[]
   end
+
+  def incomplete_tasks
+    tasks.reject(&:complete?)
+  end
   def done?
-    tasks.empty?
+    incomplete_tasks.empty?
+  end
+  def total_size
+    tasks.sum(&:size)
+  end
+  def remaining_size
+    incomplete_tasks.sum(&:size)
+  end
+
+  def completed_velocity
+    tasks.sum(&:points_toward_velocity)
+  end
+
+  def current_rate
+    completed_velocity/Project.velocity_length_in_days.to_f
+  end
+
+  def projected_days_remaining
+    remaining_size/current_rate
+  end
+
+  def on_schedule?
+    return false if projected_days_remaining.nan?
+    projected_days_remaining.days.from_now <= due_date
+  end
+
+  def self.velocity_length_in_days
+    21
   end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,25 @@
 class Task
+  attr_accessor :size, :completed_at
+  def initialize(options={})
+    @completed_at = options[:completed_at]
+    @size = options[:size]
+  end
+
+  def complete?
+    @completed_at.present?
+  end
+
+  def mark_completed (date = nil)
+    @completed_at = (date||Time.current)
+  end
+
+  def part_of_velocity?
+    return false unless complete?
+    @completed_at > Project.velocity_length_in_days.days.ago if @completed_at
+  end
+
+  def points_toward_velocity
+    part_of_velocity? ? size : 0
+  end
+
 end
